b88b757e5db3bf4e1ca9086c46974187175c1491
Artifex <krisztianka78.4@gmail.com>
2026-05-23 16:23:25 +0200

    w

--- common/Player/player.gd
+++ common/Player/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+@onready var body: AnimatedSprite2D = $Body
 
 
 const SPEED = 300.0
@@ -9,6 +10,8 @@ func _physics_process(delta: float) -> void:
 	var directionx := Input.get_axis("left", "right")
 	var directiony := Input.get_axis("up", "down")
 	if directionx or directiony:
+		if directionx:
+			body.scale.x = directionx
 		velocity.x = directionx * SPEED
 		velocity.y = directiony * SPEED
 	else:
